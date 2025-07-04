3995abd8cf6da1cbb20a7fe808827656d325db26
Clarity Anne Mayhew <154284848+ClarityAnne@users.noreply.github.com>
2025-07-04 18:47:34 +0200

    dsfdsfdfsf

--- public/spells.js
+++ public/spells.js
@@ -153,3 +153,74 @@ document.documentElement.scrollTop = 0;
 
 
         
+
+
+
+//â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡
+//â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡
+//â—†  native neocities hitcounter  â—†
+//â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡
+//â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡â—†â—‡
+
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+	if (this.readyState == 4 && this.status == 200) {
+		let site_data = JSON.parse(this.responseText);
+		let num_arr = site_data.info.views.toString().split("");
+		let num_str = "";
+
+		for (let i = 0; i < num_arr.length; i++) {
+			num_str += num_arr[i];
+			if ((num_arr.length - 1 - i) % 3 == 0 && num_arr.length - 1 - i != 0) {
+				num_str += ",";
+			}
+		}
+
+		let date_obj = new Date(site_data.info.last_updated);
+		let days = [
+			"Sunday",
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday"
+		];
+		let months = [
+			"January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December"
+		];
+		document.getElementById("lastupdate").innerHTML =
+			days[date_obj.getDay()] +
+			", " +
+			months[date_obj.getMonth()] +
+			" " +
+			date_obj.getDate() +
+			", " +
+			date_obj.getFullYear();
+
+		document.getElementById("hitcount").innerHTML = num_str;
+	}
+};
+xhttp.open(
+	"GET",
+	"https://weirdscifi.ratiosemper.com/neocities.php?sitename=midiforest",
+	true
+);
+xhttp.send();
+
+
+
+
+
+ 
